e7b7852bf0406de09089e5a0be9376926c1b1aa7
Brokker <82930116+BR0KK3R@users.noreply.github.com>
2022-08-05 16:47:22 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function about(){
+    document.getElementById('about').scrollIntoView({behavior: 'smooth'});
+}
+function projects(){
+    document.getElementById('projects').scrollIntoView({behavior: 'smooth'});
+}
+function skills(){
+    document.getElementById('skills').scrollIntoView({behavior: 'smooth'});
+}
+function contact(){
+    document.getElementById('contact').scrollIntoView({behavior: 'smooth'});
+}
+
+function openInNewTab(url) {
+    window.open(url, '_blank').focus();
+   }
